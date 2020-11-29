a40b0118205be94a6af54397c09219a1dea5c01a
Grle <grle0116@gmail.com>
2020-11-30 01:00:37 +0800

    Shooni buusan taliig hadgalah

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдшэээгр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэе.
-
 var activePlayer = 1;
-
 
 // Тоглогчдын цуглуулсан оноог хадгалах
 var scores = [0, 0];
@@ -9,13 +7,17 @@ var scores = [0, 0];
 // Тоглогчын ээлжийн оноог хадгалах хувьсагч
 var roundScore = 0;
 
+// Программ эхлэхэд бэлдэх
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".dice").style.display = "none";
+
 // Шоо буух болгонд аль талаараа буухыг хадгалах, 1-6 гэсэн утгыг хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-
-var dice = Math.floor(Math.random() * 6)+ 1;
-console.log("Шоо:" + dice);
-
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').innerHTML = 0;
-document.querySelector('#current-0').innerHTML = 0;
-
-document.querySelector('.dice').style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random() * 6)+ 1;
+    document.querySelector(".dice").style.display = "block";
+    document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
+});
